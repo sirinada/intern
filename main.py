4176e6b875ff34dfeafb0d863bcc984d0f4abcaa
sirinada <aomsin.n9@gmail.com>
2021-06-11 00:44:42 +0700

    add function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/ticket')
-def main(price:int, unit:int, payment_method:str, protect:int):
+def main(price:int, unit:int, payment_method:str, ticket_protect:str):
 
     amount = price*unit
     tic_prot = amount*0.07
@@ -12,16 +12,24 @@ def main(price:int, unit:int, payment_method:str, protect:int):
     credit_with_tic_prot = (amount+tic_prot+fee)*0.03
 
     if payment_method == 'credit':
-        if protect == 0:
+        if ticket_protect == 'no':
             total = amount+fee+credit_fee
             return {'Total price':total}
-        else:
+        elif ticket_protect == 'yes':
             total = amount+tic_prot+fee+credit_with_tic_prot
             return {'Total price':total}
-    else:
-        if protect == 0:
+        else:
+            print('error')
+
+    elif payment_method == 'cash' or payment_method == 'atm':
+        if ticket_protect == 'no':
             total = amount+fee
             return {'Total price':total}
-        else:
+        elif ticket_protect == 'yes':
             total = amount+tic_prot+fee
             return {'Total price':total}
+        else:
+            print('error')
+    
+    else:
+        print('error')
